70de46eb70a537fb829f82d9e0e6bf44cc0aeec4
hello0a <aya70713@gmail.com>
2026-03-10 12:27:28 +0900

    260310 12시 안영아

--- durudurub/src/main/java/com/aloha/durudurub/controller/HomeController.java
+++ durudurub/src/main/java/com/aloha/durudurub/controller/HomeController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.aloha.durudurub.dto.Banner;
@@ -16,6 +17,7 @@ import com.aloha.durudurub.service.CategoryService;
 /**
  * 메인 페이지 컨트롤러
  */
+@CrossOrigin("*")
 @Controller
 public class HomeController {
     
