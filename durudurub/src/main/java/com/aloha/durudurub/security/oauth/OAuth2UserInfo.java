f96b1177ca1115b78c7bfe3968e1b64111ec1c48
hello0a <aya70713@gmail.com>
2026-03-26 15:38:35 +0900

    주석 삭제

--- durudurub/src/main/java/com/aloha/durudurub/security/oauth/OAuth2UserInfo.java
+++ durudurub/src/main/java/com/aloha/durudurub/security/oauth/OAuth2UserInfo.java
@@ -5,8 +5,8 @@ import java.util.Map;
 // 공통 메서드 정의
 public interface OAuth2UserInfo {
     
-    String getProvider();   // kakao, google, naver
-    String getProviderId();     // 소셜 사용자 ID
+    String getProvider();  
+    String getProviderId();     
     String getEmail();
     String getUserName();
     
